ed1a5a5da310a0b9810344c750de688b8a6b2b00
VALADAOS <VALADAOS@CTA1NBJ92LJR3>
2025-02-28 18:56:05 -0300

    Add project files.

---
+++ 3ASystem.Application/DependenciesResolver.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace _3ASystem.Application
+{
+	public static class DependenciesResolver
+	{
+
+		public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+		{
+			//mediator
+
+			return services;
+		}
+	}
+}
---
+++ 3ASystem.Infrastructure/DependenciesResolver.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace _3ASystem.Infrastructure
+{
+	public static class DependenciesResolver
+	{
+
+		public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services)
+		{
+
+			return services;
+		}
+	}
+}
